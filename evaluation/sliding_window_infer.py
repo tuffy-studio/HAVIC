049b1012c6b23710e14dd744d9d473da3734b9fd
tuffy-studio <2395055466@qq.com>
2025-12-23 17:21:15 +0800

    fixed sliding_window_infer.py

--- evaluation/sliding_window_infer.py
+++ evaluation/sliding_window_infer.py
@@ -7,8 +7,9 @@
 # input: a csv file containing: (video_path, audio label, visual_label, overall_label) or (video_path, overall_label)
 # for labels, 1 means fake, 0 means real
 
-# output1: the statstics of the inference results, including ACC, AUC, AP, T1, T0.1. 
-# output2: a csv file containing: (video_path, audio_label, audio pred, visual_label, visual_pred, overall_label, overall_pred) or (video_path, label, pred)
+# output1: a csv file containing: (video_path, overall_label, overall_pred) for evaluation, (video_path, overall_pred) for inference
+# output2 (if the mode is evaluation): the statstics of the evaluation results, including ACC, AP, AUC 
+
 # =============================================================
 
 # --------------------------- import module / package ----------------------------------
@@ -21,7 +22,6 @@ import numpy as np
 import cv2
 
 import sys
- # 获取当前脚本所在的目录，添加环境变量
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR,"FaceX_Zoo/face_sdk"))
@@ -74,11 +74,10 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--finetune_path",
+        "--mode",
         type=str,
         required=True,
-        default="evalution",
-        help="evalution or inference mode"
+        help="evaluation or inference mode"
     )
 
     # =========================== sliding window hyperparameters ===========================
@@ -91,26 +90,13 @@ def parse_args():
     parser.add_argument("--max_time", type=int, default=10, help="Max processing time (seconds)")
     parser.add_argument("--max_workers", type=int, default=2, help="Number of workers")
 
-    parser.add_argument(
-        "--classify_loss",
-        type=str,
-        default="BCE",
-        choices=["BCE", "CE"],
-        help="Classification loss type"
-    )
-
     return parser.parse_args()
 
 args = parse_args()
 csv_file_path = args.csv_file_path
 save_csv_path = args.save_csv_path
-if not os.path.exists(save_csv_path):
-    with open(save_csv_path, mode="w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["video_name", "overall_label", "pred_score"])
-
+mode = args.mode
 finetune_path = args.finetune_path
-classify_loss = args.classify_loss
 
 # Hyperparameters for sliding window
 WINDOW_SIZE_FRAMES = args.window_size_frames       # number of frames per window
@@ -147,17 +133,17 @@ def sample_video_5fps_decord(video_path, frame_output_root='./sliding_window_inf
         print(f"Failed to retrieve FPS, skipping: {video_path}")
         return -1
 
-    duration = total_frames / fps  # 视频总时长（秒）
+    duration = total_frames / fps  # Total video duration (seconds)
 
-    # 每0.2s采样一个时间点
+    # Sample one time point every 0.2 seconds
     times = np.arange(0, duration, 0.2)
     num_sampled_frames = len(times)
-    # 转换为对应帧号
+    # Convert to corresponding frame indices
     sample_indices = (times * fps).astype(int)
-    sample_indices = np.clip(sample_indices, 0, total_frames - 1)  # 防止越界
-    sample_indices = np.unique(sample_indices)  # 去重
+    sample_indices = np.clip(sample_indices, 0, total_frames - 1) # Prevent out-of-bounds
+    sample_indices = np.unique(sample_indices)  # Remove duplicates
 
-    # 检查是否已存在相同数量的采样帧
+    # Check if the sampled frames already exists
     existing_frames = [f for f in os.listdir(frame_dir) if f.endswith('.png')]
     if len(existing_frames) >= len(sample_indices):
         #print(f"Frames already exist, skipping: {frame_dir}")
@@ -166,16 +152,14 @@ def sample_video_5fps_decord(video_path, frame_output_root='./sliding_window_inf
     frames = vr.get_batch(sample_indices).asnumpy()
     for idx, frame in enumerate(frames):
         frame_path = os.path.join(frame_dir, f"frame_{idx:02d}.png")
-        if not os.path.exists(frame_path):  # 避免覆盖
+        if not os.path.exists(frame_path):  # Avoid overwriting if the file already exists
             cv2.imwrite(frame_path, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
-            #imageio.imwrite(frame_path, frame)
         if idx > MAX_TIME*5:
             break
     
     return num_sampled_frames
 
 def load_face_detection_model():
-    # 生成绝对路径，确保无论在哪个目录调用，路径都正确
     config_path = os.path.join(BASE_DIR, 'FaceX_Zoo', 'face_sdk', 'config', 'model_conf.yaml')
     model_path = os.path.join(BASE_DIR, 'FaceX_Zoo', 'face_sdk', 'models')
 
@@ -186,7 +170,6 @@ def load_face_detection_model():
     model_category = 'face_detection'
     model_name = model_conf[scene][model_category]
 
-    # 加载模型
     faceDetModelLoader = FaceDetModelLoader(model_path, model_category, model_name, meta_file="model_meta.json")
     model, cfg = faceDetModelLoader.load_model()
     faceDetModelHandler = FaceDetModelHandler(model, 'cuda:5', cfg)
@@ -202,21 +185,19 @@ class FaceX_Zoo_FaceCropper:
 
     def extract(self, frame_path, save_path):
         """
-        从单张图像中提取第一个人脸区域并保存
+        Extract the face region from a single image and save it
         """
         frame = cv2.imread(frame_path)
         if frame is None:
-            raise ValueError(f"无法读取图像: {frame_path}")
-        
+            raise ValueError(f"can not read image: {frame_path}")
+
         dets = self.faceDetModelHandler.inference_on_image(frame)
         if len(dets) == 0:
-            raise ValueError("未检测到人脸")
-            return False
+            raise ValueError(f"fail to detect face: {frame_path}")
 
         x_min, y_min, x_max, y_max = map(int, dets[0][:4])
         H, W = frame.shape[:2]
 
-        # 缩放人脸框
         cx, cy = (x_min + x_max) / 2, (y_min + y_max) / 2
         new_w = (x_max - x_min) * self.scale
         new_h = (y_max - y_min) * self.scale
@@ -231,14 +212,13 @@ class FaceX_Zoo_FaceCropper:
         return True
 
 def extract_audio_from_video(video_file, output_audio_file):
-    """从.mp4文件中提取音频并保存为.wav格式"""
+    """Extract audio from a .mp4 file and save it as a .wav file"""
     if os.path.exists(output_audio_file):
         return
 
     try:
-        # 使用 ffmpeg 从视频中提取音频并保存为 wav 格式
         ffmpeg.input(video_file).output(output_audio_file, ac=1, ar='16k').run(
-            quiet=True)  # ac=1 表示单声道，ar='16k' 设置采样率
+            quiet=True)  
         print(f"Audio successfully extracted to {output_audio_file}.")
     except ffmpeg.Error as e:
         print(f"Error extracting audio: {e}")
@@ -277,17 +257,20 @@ from decord import VideoReader
 from decord import cpu
 
 # ---------------- helper: 单个视频推理 ----------------
-def process_single_video(index, row, classify_loss = "BCE"):
+def process_single_video(index, row, mode):
     results = {}
     try:
-        video_path = row.get("video_path", row.get("video_name"))
-        overall_label = row["overall_label"]
+        video_path = row.get("video_path")
+
+        if mode == "evaluation":
+            overall_label = row["overall_label"]
 
         # ---------------- 2.3.1 video preprocess ----------------
-        parts = video_path.split(os.sep)
-        video_name = os.path.splitext("_".join(parts[-3:]))[0]
-
         # i.extract frame from video
+
+        # Convert the full video path into a valid folder name for saving temporary files
+        # This avoids issues caused by '/' or '\' in the path
+        video_name = video_path.replace(os.sep, "_") 
         frame_output_dir = f"./sliding_window_inference_tmp/sampled_frames/{video_name}"
         os.makedirs(frame_output_dir, exist_ok=True)
 
@@ -313,9 +296,7 @@ def process_single_video(index, row, classify_loss = "BCE"):
                 save_path = os.path.join(cropped_face_dir, frame_file)
                 try:
                     if not os.path.exists(save_path):
-                        tag = face_cropper.extract(frame_path, save_path)
-                        if tag == False:
-                            continue
+                        face_cropper.extract(frame_path, save_path)
                     frame = Image.open(save_path).convert('RGB')
                     frame = tensor_process(frame)
                     face_frames_list.append(frame)
@@ -323,7 +304,7 @@ def process_single_video(index, row, classify_loss = "BCE"):
                     if count >= MAX_TIME*5:
                         break
                 except Exception as e:
-                    print(f"[Warning] {video_name}: Failed to process {frame_file}: {e}")
+                    print(f"[Warning] {video_path}: Failed to process {frame_file}: {e}")
                     return None
 
         if len(face_frames_list) < 16:
@@ -350,7 +331,6 @@ def process_single_video(index, row, classify_loss = "BCE"):
             video_windows.append(frames_tensor[start:end].permute(1, 0, 2, 3))    
         video_batch = torch.stack(video_windows)  # [num_windows, 3, T, H, W]
 
-        #total_audio_len = waveform.size(1)
         total_audio_len = int((len(face_frames_list) / total_frames) * waveform.size(1))
         segment_len = int((total_audio_len*WINDOW_SIZE_FRAMES)/(WINDOW_SIZE_FRAMES + WINDOW_STRIDE_FRAMES*(sliding_windows_num-1)))
         stride = int((WINDOW_STRIDE_FRAMES/WINDOW_SIZE_FRAMES)*segment_len)
@@ -380,70 +360,79 @@ def process_single_video(index, row, classify_loss = "BCE"):
 
         audio_batch = torch.stack([f.to(device) for f in fbank_list])
         assert audio_batch.size(0) == video_batch.size(0), "batch size mismatch!"
-        print(f"{video_name}: video_batch {video_batch.shape}, audio_batch {audio_batch.shape}")
+        print(f"{video_path}:\n video_batch {video_batch.shape}, audio_batch {audio_batch.shape}")
 
         # ---------------- 2.3.4 model forward ----------------
         with torch.inference_mode(), torch.autocast("cuda"):
             audio_outputs, visual_outputs, output = model(audio=audio_batch, video=video_batch)
-        sample_level_pred = output.detach().cpu().mean(dim=0)
+            sigmoid_output = torch.sigmoid(output)
+        sample_level_pred = sigmoid_output.detach().cpu().mean(dim=0)
 
-        results["pred"] = sample_level_pred.unsqueeze(0)
-        if classify_loss == "CE":
-            results["label"] = torch.tensor([int(overall_label)], dtype=torch.long)
+        results["overall_pred"] = sample_level_pred.unsqueeze(0)
+
+        if mode == "evaluation":
+                results["overall_label"] = torch.tensor([int(overall_label)], dtype=torch.float32)
+        results["video_path"] = video_path
+
+        if mode == "evaluation":
+            print(f"[{index+1}/{len(data)}] {video_path}: overall_label {overall_label}, overall_pred {sample_level_pred.numpy()}", flush=True)
         else:
-            results["label"] = torch.tensor([int(overall_label)], dtype=torch.float32)
+            print(f"[{index+1}/{len(data)}] {video_path}: overall_pred {sample_level_pred.numpy()}", flush=True)
         
-        results["video_name"] = video_name
-        print(f"[{index+1}/{len(data)}] {video_name}: label {overall_label}, pred {sample_level_pred.numpy()}", flush=True)
         return results
 
     except Exception as e:
-        print(f"[Error] {row['video_name']}: {e}")
+        print(f"[Error] {row['video_path']}: {e}")
         return None
 
 # ---------------- 3. 多线程执行 ----------------
 csv_records = []
 all_overall_pred, all_overall_label = [], []
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:  # 改成你机器的CPU核心数
-    futures = [executor.submit(process_single_video, i, row, classify_loss) for i, row in enumerate(data)]
+    futures = [executor.submit(process_single_video, i, row, mode) for i, row in enumerate(data)]
     for future in tqdm(as_completed(futures), total=len(futures), desc="Sliding Window Inference"):
         res = future.result()
         if res is not None:
-            # res 包含: {"video_name": str, "pred": tensor, "label": tensor}
-            video_name = res["video_name"]
-            pred_score = res["pred"].item() if torch.is_tensor(res["pred"]) else float(res["pred"])
-            overall_label = int(res["label"]) if not torch.is_tensor(res["label"]) else int(res["label"].item())
+            # res: {"video_path": str,  "overall_label": tensor, "overall_pred": tensor}
+            video_path = res["video_path"]
+            overall_pred = res["overall_pred"].item() if torch.is_tensor(res["overall_pred"]) else float(res["overall_pred"])
+            all_overall_pred.append(overall_pred)
 
-            # 保存到列表
-            all_overall_pred.append(pred_score)
-            all_overall_label.append(overall_label)
+            if mode == "evaluation":
+                overall_label = int(res["overall_label"]) if not torch.is_tensor(res["overall_label"]) else int(res["overall_label"].item())
+                all_overall_label.append(overall_label)
 
-            # 记录到 CSV
-            csv_records.append({
-                "video_name": video_name,
-                "overall_label": overall_label,
-                "pred_score": pred_score
-            })
+            
+            # write into CSV
+            if mode == "evaluation":
+                csv_records.append({
+                    "video_path": video_path,
+                    "overall_label": overall_label,
+                    "overall_pred": overall_pred
+                })
+            else:
+                csv_records.append({
+                    "video_path": video_path,
+                    "overall_pred": overall_pred
+                })
 
-            with open(save_csv_path, mode="w", newline="") as f:
-                writer = csv.DictWriter(f, fieldnames=["video_name", "overall_label", "pred_score"])
-                writer.writeheader()
-                writer.writerows(csv_records)
+    with open(save_csv_path, mode="w", newline="") as f:
+        if mode=="evaluation":
+            fieldnames = ["video_path", "overall_label", "overall_pred"]
+        else:
+            fieldnames = ["video_path", "overall_pred"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(csv_records)
 
-print(f"推理结果已保存到: {save_csv_path}")
-
-# ---------------- 4. 计算指标 ----------------
-overall_output = torch.tensor(all_overall_pred, dtype=torch.float32)  # [N,1]
-overall_label  = torch.tensor(all_overall_label, dtype=torch.float32) # [N,1]
+print(f"Inference results have been saved to: {save_csv_path}")
 
 
-if classify_loss == "CE":
-    plot_classwise_logits_histogram(overall_output, overall_label, normalize=True,
-        save_path="./sliding_window_inference_logs/classwise_logits.png"
-    )
-    one_hot_target = F.one_hot(overall_label, num_classes=2).float()
-    stats = calculate_stats(overall_output.cpu(), one_hot_target.cpu())
-else:
+# ---------------- 4. caculate the statstics of the evaluation results ----------------
+if mode == "evaluation":
+    overall_output = torch.tensor(all_overall_pred, dtype=torch.float32)  # [N,1]
+    overall_label  = torch.tensor(all_overall_label, dtype=torch.float32) # [N,1]
+
     plot_classwise_logits_histogram_bce(
         overall_output, overall_label, normalize=False,
         save_path="./sliding_window_inference_logs/classwise_logits.png"
@@ -451,8 +440,8 @@ else:
     overall_label = overall_label.unsqueeze(1)
     stats = calculate_stats(torch.sigmoid(overall_output).cpu(), overall_label.cpu())
 
-for i, s in enumerate(stats):
-    print(f"Class {i} - ACC: {s['ACC']:.4f}, AP: {s['AP']:.4f}, AUC: {s['AUC']:.4f}, T1: {s['T1']:.4f}, T0.1: {s['T0.1']:.4f}, f1: {s['F1']:.4f}")
+    for i, s in enumerate(stats):
+        print(f"ACC: {s['ACC']:.4f}, AP: {s['AP']:.4f}, AUC: {s['AUC']:.4f}, T1: {s['T1']:.4f}, T0.1: {s['T0.1']:.4f}, f1: {s['F1']:.4f}")
 
 
 
