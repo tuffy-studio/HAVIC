6cba88585708a70b90ada38e4cde23cc27edf7e3
tuffy-studio <2395055466@qq.com>
2026-04-19 18:49:13 +0800

    Run and verify 'initialize ft weights' pipeline.

--- weights/initialize_finetune_weights.py
+++ weights/initialize_finetune_weights.py
@@ -8,9 +8,8 @@ from src.models.HAVIC import *
 from collections import OrderedDict
 
 
-input_weight_path = "./weights/pt_model.200.pth"
-
-output_weight_path = "./weights/model_to_be_ft.pth"
+input_weight_path = "./pt_model.200.pth" # path to the pretrained model weights
+output_weight_path = "./model_to_be_ft.pth" # path to save the initialized finetuning weights (after remapping and filtering)
 
 pt_weight = torch.load(input_weight_path)
 
@@ -18,20 +17,20 @@ ft_model = HAVIC_FT()
 
 ft_weight = OrderedDict()
 for k in pt_weight.keys():
-    if ('encoder' in k) or ('AudioVisualInteractionModule' in k):
+    # only keep the weights related to the audio/visual encoder and the audio-visual interaction module for finetuning
+    if ('encoder' in k) or ('AudioVisualInteractionModule' in k): 
         ft_weight[k] = pt_weight[k]
     else:
         continue
         
-missing, unexpected = ft_model.load_state_dict(ft_weight, strict=False)
+missing, _ = ft_model.load_state_dict(ft_weight, strict=False)
 
-print('\n' + '=' * 50)
+print('=' * 50)
 print("Missing keys (weights need to train from scratch in finetuing stage):")
 print(missing)
-
-print('\n' + '=' * 50)
-print("Unexpected keys:")
-print(unexpected)
+print('=' * 50)
 
 state_dict = ft_model.state_dict()
 torch.save(state_dict, output_weight_path)
+
+print(f"Initialized weights saved to: {output_weight_path}")
